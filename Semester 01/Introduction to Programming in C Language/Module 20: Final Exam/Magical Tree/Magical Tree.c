2bf0dd5b53067b6b5abbfac309fc3f22847c4d3d
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-05-17 00:44:07 +0600

    Update Magical Tree.c

--- Semester 01/Introduction to Programming in C Language/Module 20: Final Exam/Magical Tree/Magical Tree.c
+++ Semester 01/Introduction to Programming in C Language/Module 20: Final Exam/Magical Tree/Magical Tree.c
@@ -1,1 +1,32 @@
+#include<stdio.h>
+int main(){
+    int N;
+    
+    scanf("%d", &N);
+    if(N % 2 != 0){
+        int n = N / 2;
+        
+        for(int r = 1; r <= 6 + n; r++){
+            for(int s = 6 + n - r; s >= 1; s--){
+                printf(" ");
+            }
+            for(int c = 1; c < r * 2; c++){
+                printf("*");
+            }
+            printf("\n");
+        }
+        
+        for(int l = 1; l <= 5; l++){
+            for(int s = 1; s <= 5; s++){
+                printf(" ");
+            }
+            
+            for(int p = 1; p <= N; p++){
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
 
+    return 0;
+}
