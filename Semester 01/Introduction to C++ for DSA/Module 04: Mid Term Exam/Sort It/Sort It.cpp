90913eae21a56d3a63a9df4cb20d37630bb19261
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-05-26 01:37:33 +0600

    Create Sort It.cpp

---
+++ Semester 01/Introduction to C++ for DSA/Module 04: Mid Term Exam/Sort It/Sort It.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<long long> A(N);
+    
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+    
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+    
+    cout << "\n";
+
+    reverse(A.begin(), A.end());
+
+    for (int i = 0; i < N; i++) {
+        cout << A[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
