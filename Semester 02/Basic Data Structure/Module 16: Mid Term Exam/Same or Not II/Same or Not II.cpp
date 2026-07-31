62e55bc6565ab4f527edbb860be36a989ba6e855
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-07-31 19:24:19 +0600

    Create Same or Not II.cpp

---
+++ Semester 02/Basic Data Structure/Module 16: Mid Term Exam/Same or Not II/Same or Not II.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack{
+public:
+    int arr[1005];
+    int topIndex = -1;
+
+    void push(int x){
+        arr[++topIndex] = x;
+    }
+
+    void pop(){
+        topIndex--;
+    }
+
+    int top(){
+        return arr[topIndex];
+    }
+
+    bool empty() {
+        return topIndex == -1;
+    }
+};
+
+class Queue{
+public:
+    int arr[1005];
+    int frontIndex = 0, rearIndex = 0;
+
+    void push(int x) {
+        arr[rearIndex++] = x;
+    }
+
+    void pop(){
+        frontIndex++;
+    }
+
+    int front() {
+        return arr[frontIndex];
+    }
+
+    bool empty() {
+        return frontIndex == rearIndex;
+    }
+};
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+
+    Stack A;
+    Queue B;
+
+    for (int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        A.push(x);
+    }
+
+    for (int i = 0; i < M; i++){
+        int x;
+        cin >> x;
+        B.push(x);
+    }
+
+    if (N != M){
+        cout << "NO";
+        return 0;
+    }
+
+    while (!A.empty()) {
+        if (A.top() != B.front()) {
+            cout << "NO";
+            return 0;
+        }
+        A.pop();
+        B.pop();
+    }
+
+    cout << "YES";
+
+    return 0;
+}
